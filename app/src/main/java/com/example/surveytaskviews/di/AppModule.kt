e306159083302bf115d1584245394ca2767cb4da
mdshahariazrimon <116777348+mdshahariazrimon@users.noreply.github.com>
2025-07-16 01:29:30 +0600

    Dependency Injection

--- app/src/main/java/com/example/surveytaskviews/di/AppModule.kt
+++ app/src/main/java/com/example/surveytaskviews/di/AppModule.kt
@@ -1,4 +1,32 @@
 package com.example.surveytaskviews.di
 
-class AppModule {
+import com.example.surveytaskviews.data.network.ApiService
+import com.example.surveytaskviews.data.repository.FormRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://api.jsonbin.io/v3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+
+    // Hilt will automatically know how to provide FormRepository
+    // because its constructor is annotated with @Inject.
 }
