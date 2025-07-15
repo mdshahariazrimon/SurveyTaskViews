8a495d4a62742f06bcd67b928ed5d92a185f02fe
mdshahariazrimon <116777348+mdshahariazrimon@users.noreply.github.com>
2025-07-16 01:27:24 +0600

    JSON structure

--- app/src/main/java/com/example/surveytaskviews/data/model/FormModels.kt
+++ app/src/main/java/com/example/surveytaskviews/data/model/FormModels.kt
@@ -1,4 +1,25 @@
 package com.example.surveytaskviews.data.model
 
-class FormModels {
-}
+import com.google.gson.annotations.SerializedName
+
+data class FormResponse(
+    @SerializedName("record")
+    val record: List<Question>
+)
+
+data class Question(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("type")
+    val type: String,
+    @SerializedName("label")
+    val label: String,
+    @SerializedName("options")
+    val options: List<String>?,
+    @SerializedName("referTo")
+    val referTo: String,
+    @SerializedName("skip")
+    val skip: String,
+    @SerializedName("regex")
+    val regex: String?
+)
