d98f55ff79ed0f74dbc5294bf3216f419c9a1f8a
mdshahariazrimon <116777348+mdshahariazrimon@users.noreply.github.com>
2025-07-16 01:28:11 +0600

    defines the API endpoint using Retrofit

--- app/src/main/java/com/example/surveytaskviews/data/network/ApiService.kt
+++ app/src/main/java/com/example/surveytaskviews/data/network/ApiService.kt
@@ -1,4 +1,9 @@
 package com.example.surveytaskviews.data.network
 
-class ApiService {
+import com.example.surveytaskviews.data.model.FormResponse
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("b/687374506063391d31aca23a")
+    suspend fun getForm(): FormResponse
 }
