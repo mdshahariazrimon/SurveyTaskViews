eae1400f67f73a387bbe66d4c36dccf87c2e0bb4
mdshahariazrimon <116777348+mdshahariazrimon@users.noreply.github.com>
2025-07-16 01:28:53 +0600

    API for data access

--- app/src/main/java/com/example/surveytaskviews/data/repository/FormRepository.kt
+++ app/src/main/java/com/example/surveytaskviews/data/repository/FormRepository.kt
@@ -1,4 +1,12 @@
 package com.example.surveytaskviews.data.repository
 
-class FormRepository {
+import com.example.surveytaskviews.data.model.FormResponse
+import com.example.surveytaskviews.data.network.ApiService
+import javax.inject.Inject
+
+// Implementation of the repository
+class FormRepository @Inject constructor(private val apiService: ApiService) {
+    suspend fun getForm(): FormResponse {
+        return apiService.getForm()
+    }
 }
